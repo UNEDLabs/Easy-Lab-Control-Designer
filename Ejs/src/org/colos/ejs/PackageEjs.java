b21b738480d657ce8aaf4ecd82127d39d74ff3b8
marcoantmarsan <marcoa@pi.uhu.es>
2018-10-30 18:14:17 +0100

    errors in the path

--- Ejs/src/org/colos/ejs/PackageEjs.java
+++ Ejs/src/org/colos/ejs/PackageEjs.java
@@ -13,10 +13,10 @@ public class PackageEjs {
 //  static private final String VERBOSE = "-v";
   
   static private final String LOCALES = VERBOSE + 
-          " -o Ejs/distribution/bin/locales.jar -m .;ejs_lib.jar org.colos.ejs._EjsSConstants"+
-          " -s Ejs/resources/ca_CA -s Ejs/resources/da_DK -s Ejs/resources/de_DE -s Ejs/resources/es_ES -s Ejs/resources/fr_FR "+
-          " -s Ejs/resources/gr_GR -s Ejs/resources/id_ID -s Ejs/resources/nl_NL -s Ejs/resources/pl_PL -s Ejs/resources/ru_RU "+
-          " -s Ejs/resources/si_SI -s Ejs/resources/zh_CN -s Ejs/resources/zh_TW  -s Ejs/resources/ar_DZ "+
+          " -o ../Ejs/distribution/bin/locales.jar -m .;ejs_lib.jar org.colos.ejs._EjsSConstants"+
+          " -s ../Ejs/resources/ca_CA -s ../Ejs/resources/da_DK -s ../Ejs/resources/de_DE -s ../Ejs/resources/es_ES -s ../Ejs/resources/fr_FR "+
+          " -s ../Ejs/resources/gr_GR -s ../Ejs/resources/id_ID -s ../Ejs/resources/nl_NL -s ../Ejs/resources/pl_PL -s ../Ejs/resources/ru_RU "+
+          " -s ../Ejs/resources/si_SI -s ../Ejs/resources/zh_CN -s ../Ejs/resources/zh_TW  -s ../Ejs/resources/ar_DZ "+
           " org/colos/ejs/osejs/++";
 
 //  static final String LOOK_AND_FEEL = VERBOSE + 
@@ -26,28 +26,28 @@ public class PackageEjs {
 //  " com/digitprop/tonic/++";
 
   static final String EJS_CONSOLE = VERBOSE +  
-          " -o Ejs/distribution/EjsConsole.jar "+
+          " -o ../Ejs/distribution/ELCDConsole.jar "+
           " -m .;bin/locales.jar;bin/ejs_lib.jar;bin/osp.jar;bin/ejs_extras_java8.jar;bin/ejs_extras_java9.jar"+
           " org.colos.ejs.osejs.EjsConsole"+
-          " -s Ejs/bin -s Ejs/distribution/bin/osp.jar -s Ejs/distribution/bin/bcel.jar -c Ejs/libraries/unpackaged/classes"+
-          " -x Ejs/distribution/bin/bcel.jar "+
+          " -s ../Ejs/bin -s ../Ejs/distribution/bin/osp.jar -s ../Ejs/distribution/bin/bcel.jar -c ../Ejs/libraries/unpackaged/classes"+
+          " -x ../Ejs/distribution/bin/bcel.jar "+
           " -x ++Thumbs.db"+
   " org/colos/ejs/osejs/EjsConsole++.class org/colos/ejs/osejs/MacOSXHandler.class org/colos/ejs/osejs/resources/++"+
   " org/opensourcephysics/resources/++ data/icons/EjsLogo.gif data/icons/EjsSLogo.png data/icons/ConsoleIcon.gif data/icons/edit.gif";
 
   static final String EJS = VERBOSE +
-          " -o Ejs/distribution/bin/ejs.jar  -m .;ejs_lib.jar;osp.jar;bsh.jar org.colos.ejs._EjsSConstants"+
-          " -s Ejs/bin -c Ejs/libraries/unpackaged/classes"+
-          " -c Ejs/libraries/HotEqn.jar -c Ejs/distribution/bin/osp.jar"+
-          " -x Ejs/distribution/bin/osp.jar -x Ejs/distribution/bin/ejs_lib.jar -x org/colos/ejs/PackageEjs.class -x ++Thumbs.db"+
+          " -o ../Ejs/distribution/bin/ejs.jar  -m .;ejs_lib.jar;osp.jar;bsh.jar org.colos.ejs._EjsSConstants"+
+          " -s ../Ejs/bin -c ../Ejs/libraries/unpackaged/classes"+
+          " -c ../Ejs/libraries/HotEqn.jar -c ../Ejs/distribution/bin/osp.jar"+
+          " -x ../Ejs/distribution/bin/osp.jar -x ../Ejs/distribution/bin/ejs_lib.jar -x org/colos/ejs/PackageEjs.class -x ++Thumbs.db"+
           " -x logback.xml "+
   " org/colos/ejs/++ org/colos/ejss/++ data/++ com/++.properties com/hexidec/ekit/icons/++"; // jtidy.properties org/w3c/tidy/++.properties";
   
   static final String COM_SUN = VERBOSE +
-  " -o distribution/bin/comSun9.jar -v -m . org.colos.ejs._EjsSConstants"+
-  " -c distribution/bin/osp.jar  "+
-  " -s libraries/tools.jar "+
-  " -x distribution/bin/osp.jar "+
+  " -o ../Ejs/distribution/bin/comSun9.jar -v -m . org.colos.ejs._EjsSConstants"+
+  " -c ../Ejs/distribution/bin/osp.jar  "+
+  " -s ../Ejs/libraries/tools.jar "+
+  " -x ../Ejs/distribution/bin/osp.jar "+
   " com/sun/tools/javac/Main.class com/sun/tools/javac/resources/++";
   
   /**
